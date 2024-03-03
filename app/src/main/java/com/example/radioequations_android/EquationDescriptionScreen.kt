6c48c05807e316453d412797c8e8b98d3d974542
Robby Sarvis <rjs.app.dev@gmail.com>
2024-03-03 14:40:43 -0600

    refactor equation descriptions since they are separate composable items

--- app/src/main/java/com/example/radioequations_android/EquationDescriptionScreen.kt
+++ app/src/main/java/com/example/radioequations_android/EquationDescriptionScreen.kt
@@ -1,16 +1,26 @@
 package com.example.radioequations_android
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import com.vkochenkov.equationdisplayerlib.EquationItem
 import models.Equation
 
 @Composable
 fun EquationDescriptionScreen(equation: Equation) {
-    Column(modifier = Modifier.fillMaxWidth()) {
-        equation.title.Show()
-
-
+    Column(modifier = Modifier
+        .verticalScroll(rememberScrollState())
+        .fillMaxSize()) {
+        equation.description.forEach {item ->
+            when (item) {
+                is String -> Text(text = item)
+                is EquationItem -> item.Show()
+            }
+        }
     }
 }
--- app/src/main/java/models/Equation.kt
+++ app/src/main/java/models/Equation.kt
@@ -4,7 +4,7 @@ import com.vkochenkov.equationdisplayerlib.EquationItem
 
 data class Equation(
     val title: EquationItem,
-    val description: List<String>,
+    val description: List<Any>,
     val id: EquationId,
     val filters: List<EquationFilterVariant>
 )
