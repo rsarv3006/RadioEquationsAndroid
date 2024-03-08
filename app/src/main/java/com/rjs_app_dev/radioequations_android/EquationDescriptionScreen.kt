22c44bc1f475f579dbf82274b1b14d3b581add48
Robby Sarvis <rjs.app.dev@gmail.com>
2024-03-08 15:55:05 -0600

    padding on description screen

--- app/src/main/java/com/rjs_app_dev/radioequations_android/EquationDescriptionScreen.kt
+++ app/src/main/java/com/rjs_app_dev/radioequations_android/EquationDescriptionScreen.kt
@@ -2,11 +2,13 @@ package com.rjs_app_dev.radioequations_android
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.vkochenkov.equationdisplayerlib.EquationItem
 import models.Equation
 
@@ -14,6 +16,7 @@ import models.Equation
 fun EquationDescriptionScreen(equation: Equation) {
     Column(modifier = Modifier
         .verticalScroll(rememberScrollState())
+        .padding(16.dp)
         .fillMaxSize()) {
         equation.description.forEach {item ->
             when (item) {
